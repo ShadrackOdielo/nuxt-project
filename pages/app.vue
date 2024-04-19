45fdc4b0616954ebf2bfacc6376e19fb4710cf20
ShadrackOdielo <shadrackodielo@gmail.com>
2024-04-19 19:00:31 +0300

    Update app.vue and package.json, add tailwindcss module

---
+++ pages/app.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/projects">projects</NuxtLink></li>
+          <li><NuxtLink to="/blog">articles</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
---
+++ pages/projects.vue
@@ -0,0 +1,6 @@
+<template>
+    <section>
+      <p>This page will be displayed at the /projects route.</p>
+    </section>
+  </template>
+  
